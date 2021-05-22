3713b3087704d6f75252d828f69168d55ca8ee5f
Onjomba Felix <73028636+Developer-Felix@users.noreply.github.com>
2021-05-21 22:02:12 -0700

    Update admin.py
    Admin customization

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Appointment, Doctor
 
+admin.site.site_title = "Ceder Court Interview"
+admin.site.site_header = "Ceder Court Interview"
 
 class AppointmentAdmin(admin.ModelAdmin):
     list_display = ['doctor', 'date', 'timeslot', 'patient_name']
